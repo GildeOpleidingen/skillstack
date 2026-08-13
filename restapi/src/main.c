03aeb76a08c4007d742660746200a5b63fa8b1d3
RayIT <rmarx@rayit.com>
2026-08-13 09:27:25 +0200

    Trying to connect to DB with restapi

--- restapi/src/main.c
+++ restapi/src/main.c
@@ -5,19 +5,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <mysql/mysql.h>
 
-static void on_request(struct evhttp_request *req, void *arg)
+// Test mysql
+int mysqlTest()
 {
+        MYSQL *con = mysql_init(NULL);
+
+        if (con == NULL) {
+                fprintf(stderr, "%s\n", mysql_error(con));
+                exit(1);
+        }
+
+        if (mysql_real_connect(con, "localhost", "root", "", NULL, 0, NULL, 0) == NULL) {
+                fprintf(stderr, "%s\n", mysql_error(con));
+                mysql_close(con);
+                exit(1);
+        }
+
+        if (mysql_query(con, "CREATE DATABASE testdb")) {
+                fprintf(stderr, "%s\n", mysql_error(con));
+                mysql_close(con);
+                exit(1);
+        }
+
+        mysql_close(con);
+        return 0;
+}
+
+// Handle requests
+static void on_request(struct evhttp_request *req, void *arg)
+{    
+        struct evhttp_uri *decoded = NULL;
         const char *method = evhttp_request_get_command(req) == EVHTTP_REQ_GET ? "GET" : "OTHER";
         (void)arg;
+
+        // Get Uri
+        decoded = evhttp_uri_parse(evhttp_request_get_uri(req));
+        if (!decoded) {
+                evhttp_send_error(req, HTTP_BADREQUEST, 0);
+                return;
+        }
+
+        const char *path = evhttp_uri_get_path(decoded);
+        printf("Path: %s", path);        
 
         struct evkeyvalq *headers_in = evhttp_request_get_input_headers(req);
 
         // Simple response
         struct evbuffer *buf = evbuffer_new();
         evbuffer_add_printf(buf,
-                        "{ \"ok\": true, \"method\": \"%s\", \"msg\": \"evhttp server running\" }\n",
-                        method);
+                        "{ \"ok\": true, \"method\": \"%s\", \"msg\": \"evhttp server running\", \"path\": \"%s\" }\n",
+                        method, path);
 
         evhttp_add_header(evhttp_request_get_output_headers(req), "Content-Type", "application/json");
         evhttp_send_reply(req, 200, "OK", buf);
@@ -26,6 +65,9 @@ static void on_request(struct evhttp_request *req, void *arg)
 
 int main(int argc, char **argv) 
 {
+        // test Mysql
+        mysqlTest();
+
         int port = 8080;
 
         if (argc >= 2) port = atoi(argv[1]);
